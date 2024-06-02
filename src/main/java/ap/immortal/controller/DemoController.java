d4603d4d5cfe2b945b18ca6d93c78d1b08041efd
immortal328 <amar.pujari28@gmail.com>
2024-06-02 23:37:32 +0530

    created 1st GET call which returning Thymeleaf page

---
+++ src/main/java/ap/immortal/controller/DemoController.java
@@ -0,0 +1,19 @@
+package ap.immortal.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		theModel.addAttribute("Date", LocalDateTime.now());
+		return "helloworld";
+	}
+	
+
+}
