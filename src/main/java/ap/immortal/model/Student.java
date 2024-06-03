d04a1d4a495bff4ebc5342f9b88b998d10ecfc9c
immortal328 <amar.pujari28@gmail.com>
2024-06-03 23:00:47 +0530

    Added Dropdown in student from and accessing values

--- src/main/java/ap/immortal/model/Student.java
+++ src/main/java/ap/immortal/model/Student.java
@@ -1,10 +1,15 @@
 package ap.immortal.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 	private String firstName;
 	private String lastName;
+	private String country;
 
 }
