34b519aff98036f235ee9fef2ce9a3747707e948
immortal328 <amar.pujari28@gmail.com>
2024-06-04 00:54:20 +0530

    Added Integer Range validation

--- src/main/java/ap/immortal/controller/StudentController.java
+++ src/main/java/ap/immortal/controller/StudentController.java
@@ -3,10 +3,13 @@ package ap.immortal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,6 +27,19 @@ public class StudentController {
 	
 	@Value("${systems}")
 	private List<String>  systems; 
+	
+	
+	/*
+	 * This Methid handle empty String validation
+	 * Added an initbinder ... to convert trim input strings
+     * Remove leading and trailing whitespace
+     * Resolve issue for our validation
+	 **/
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 	
 	
 	@GetMapping("/")
--- src/main/java/ap/immortal/model/Student.java
+++ src/main/java/ap/immortal/model/Student.java
@@ -1,6 +1,8 @@
 package ap.immortal.model;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,11 @@ public class Student {
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
+	
+	@Min(value=1, message="must be greater than or equal to 1")
+    @Max(value=10, message="must be less than or equal to 10")
+    private int std;
+	 
 	private String country;
 	private String favoriteLanguage;
 	private String operatingSystem;
