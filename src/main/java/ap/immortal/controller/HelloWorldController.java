820a7980020c45ce2fe1d661d340ff9528d56720
immortal328 <amar.pujari28@gmail.com>
2024-06-03 19:10:08 +0530

    Used @RequestParam to fetch values form and setting in Model

--- src/main/java/ap/immortal/controller/HelloWorldController.java
+++ src/main/java/ap/immortal/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package ap.immortal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -14,17 +15,37 @@ public class HelloWorldController {
 		return "helloworld-form";
 	}
 	
+	/**
+	 * Using ${param.studentName} to print
+	 * */
 	@GetMapping("/processForm")
 	public String processForm() {
-		return "helloworlds";
+		return "helloworlds";	
 	}
 	
+	/**
+	 * Method to process values of form
+	 * using @HttpServletRequest req. and Set in @Model
+	 * */
 	@GetMapping("/v2/processForm")
 	public String processFormV2(HttpServletRequest req , Model model) {		
 		String name = req.getParameter("studentName");
 		name = name.toUpperCase();
 		String msg = "Yoo...! "+ name;
 		model.addAttribute("msg", msg);		
+		return "helloworlds";
+	}
+	
+	/**
+	 * Method to process values of form
+	 * using @RequestParam and Set in @Model
+	 * */
+	@GetMapping("/requestParam/v2/processForm")
+	public String processFormV2(@RequestParam String studentName , Model model) {		
+		studentName = studentName.toUpperCase();
+		String msg = "Yoo...! "+ studentName;
+		model.addAttribute("msg", msg);	
+			
 		return "helloworlds";
 	}
 
