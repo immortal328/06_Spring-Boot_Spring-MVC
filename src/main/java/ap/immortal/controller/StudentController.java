e5276044ac1e1481d1758ac0179f513468fde0d2
immortal328 <amar.pujari28@gmail.com>
2024-06-04 00:37:28 +0530

    added form validatio for firstName and LastName

--- src/main/java/ap/immortal/controller/StudentController.java
+++ src/main/java/ap/immortal/controller/StudentController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import ap.immortal.model.Student;
+import jakarta.validation.Valid;
 
 @Controller
 public class StudentController {
@@ -24,7 +26,7 @@ public class StudentController {
 	private List<String>  systems; 
 	
 	
-	@GetMapping("/showStudentForm")
+	@GetMapping("/")
 	public String showStudentsForm(Model model) {	
 		model.addAttribute("student", new Student());
 		model.addAttribute("countries",countries);
@@ -35,9 +37,14 @@ public class StudentController {
 	
 	
 	@PostMapping("/processStudentForm")
-	public String processStudentForm(@ModelAttribute("student") Student student) {		
-		System.out.println("Student Details : " + student.getFirstName()+" "+student.getLastName());		
-		return "student/student-confirmation";
+	public String processStudentForm(@Valid @ModelAttribute("student") Student student, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+            return "student/student-form";
+        }
+        else {
+    		System.out.println("Student Details : " + student.getFirstName()+" "+student.getLastName());
+            return "student/student-confirmation";
+        }
 	}
 
 }
--- src/main/java/ap/immortal/model/Student.java
+++ src/main/java/ap/immortal/model/Student.java
@@ -1,5 +1,8 @@
 package ap.immortal.model;
 
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +10,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Student {	
+public class Student {
+	
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
 	private String firstName;
+	 
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
 	private String lastName;
 	private String country;
 	private String favoriteLanguage;
