4bee7c5b83db09757ad21da89de9edf2f47878ae
immortal328 <amar.pujari28@gmail.com>
2024-06-03 23:37:15 +0530

    reading countries and languages from yml. using thymeleaf loop to display on from

--- src/main/java/ap/immortal/controller/StudentController.java
+++ src/main/java/ap/immortal/controller/StudentController.java
@@ -1,5 +1,8 @@
 package ap.immortal.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,18 @@ import ap.immortal.model.Student;
 @Controller
 public class StudentController {
 	
+	@Value("${countries}")
+	private List<String>  countries;
+	
+	@Value("${languages}")
+	private List<String>  languages;
+	
+	
 	@GetMapping("/showStudentForm")
 	public String showStudentsForm(Model model) {	
 		model.addAttribute("student", new Student());
+		model.addAttribute("countries",countries);
+		model.addAttribute("languages",languages);
 		return "student/student-form";
 	}
 	
--- src/main/java/ap/immortal/model/Student.java
+++ src/main/java/ap/immortal/model/Student.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Student {
+public class Student {	
 	private String firstName;
 	private String lastName;
 	private String country;
