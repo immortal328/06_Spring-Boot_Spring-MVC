83e648a958c7f7b8c78a208c815aed8a29a4b213
immortal328 <amar.pujari28@gmail.com>
2024-06-04 01:03:33 +0530

    added regular Expression validaiton

--- src/main/java/ap/immortal/model/Student.java
+++ src/main/java/ap/immortal/model/Student.java
@@ -4,6 +4,7 @@ package ap.immortal.model;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,6 +26,9 @@ public class Student {
 	@Min(value=1, message="must be greater than or equal to 1")
     @Max(value=10, message="must be less than or equal to 10")
     private int std;
+	
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 	 
 	private String country;
 	private String favoriteLanguage;
