8fe1b48ece3c0ef2eda39afd97e95901c3c4fcc3
immortal328 <amar.pujari28@gmail.com>
2024-06-05 00:21:09 +0530

    Update PostsalCode.java

--- src/main/java/ap/immortal/validation/PostsalCode.java
+++ src/main/java/ap/immortal/validation/PostsalCode.java
@@ -1,12 +1,12 @@
 package ap.immortal.validation;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 @Constraint(validatedBy = CourseCodeConstraintValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD})
